ab8daa262c74142cadaa35e787538c04d82008fc
Johan <johanbudi1999@gmail.com>
2019-11-03 23:40:43 +0700

    Jobsheet 5

---
+++ app/src/main/java/polinema/ac/id/starterchapter05/Activities/TugasActivity.java
@@ -0,0 +1,26 @@
+package polinema.ac.id.starterchapter05.Activities;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import polinema.ac.id.starterchapter05.R;
+
+public class TugasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tugas);
+    }
+
+    public void handlerClickPushup(View view) {
+    }
+
+    public void handlerClickdips(View view) {
+    }
+
+    public void handlerClickhandstand(View view) {
+    }
+}
